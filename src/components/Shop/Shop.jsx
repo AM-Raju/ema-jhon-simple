ba6f54662086dd3bb68f1832a4c128fc47590937
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-03-29 22:03:04 +0600

    Third commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,6 +5,7 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("products.json")
@@ -12,15 +13,21 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product product={product} key={product.id}></Product>
+          <Product product={product} key={product.id} handleAddToCart={handleAddToCart}></Product>
         ))}
       </div>
       <div className="cart-container">
         <h4>Order summary</h4>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
